5e6b23fbd8370a5048241bb1c005febc6fdc7996
DevGisoun <devgisoun@gmail.com>
2025-09-22 02:11:43 +0000

    [chore] Swagger UI 내에서 확인 가능한 API 주석 작성.

--- main.py
+++ main.py
@@ -17,5 +17,8 @@ def read_hello(hello_id: int, q: Union[str, None] = None):
 
 @app.get('/stock/{stock_number}')
 def read_stock(stock_number: int):
+    """
+    주식 번호를 입력하면 기업명을 반환합니다.
+    """
     stocks = {1: '삼성전자', 2: 'SK하이닉스', 3: '네이버'}
     return {'stock_number': stock_number, 'name': stocks[stock_number]}
