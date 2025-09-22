d053d539d6517d17fcd00d77c0145efcfc4000cd
DevGisoun <devgisoun@gmail.com>
2025-09-22 01:49:31 +0000

    [feat] Hello World
    로컬 실행 명령어: uv run uvicorn main:app --reload

---
+++ main.py
@@ -0,0 +1,21 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/hello/{hello_id}")
+def read_hello(hello_id: int, q: Union[str, None] = None):
+    return {"hello_id": hello_id, "q": q}
+
+
+@app.get('/stock/{stock_number}')
+def read_stock(stock_number: int):
+    stocks = {1: '삼성전자', 2: 'SK하이닉스', 3: '네이버'}
+    return {'stock_number': stock_number, 'name': stocks[stock_number]}
